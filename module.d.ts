9b0d4f1e351b526ee97a8164b6c05209df19264a
Adnan Zaki <adnan_zaki@hotmail.co.id>
2023-03-24 08:53:01 +0700

    Fix module declaration on node packages import

--- module.d.ts
+++ module.d.ts
@@ -1,6 +1,4 @@
 import { usePaging } from "./src/ss-paging-esm";
 import { usePagingStore } from './src/ss-paging-store'
 
-declare module 'ss-paging-vue' {
-  export { usePaging, usePagingStore }
-}
+export { usePaging, usePagingStore }
